429bbd2c6eb9733f6ecf04da3f098844f0d156fe
Piemicah <nuges1.62@gmail.com>
2025-04-28 22:38:43 +0100

    add email

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
+import emailjs from "@emailjs/browser";
 
 const Contact = () => {
+  const [info, setInfo] = useState({
+    oruko: "",
+    email: "",
+    phone: "",
+    message: "",
+  });
+
+  const sendEmail = () => {
+    emailjs.init({ publicKey: "-rBRte5_kjLnvxObb" });
+    emailjs.send("service_bf9vrvd", "template_i933303", {
+      title: `Message from ${info.oruko}`,
+      name: info.oruko,
+      message: `${info.message}\n ${info.phone}`,
+      email_from: info.oruko,
+      email: info.email,
+    });
+  };
+
+  const infoHandler = (e: any) => {
+    setInfo({ ...info, [e.target.name]: e.target.value });
+  };
+
   return (
     <div id="contact" className="text-black bg-white font-mont">
       <div className="flex flex-col items-center gap-24 pt-12 bg-gradbg ">
@@ -10,28 +33,44 @@ const Contact = () => {
         <div className="flex flex-col items-center w-[80vw] font-mont font-bold text-[12px] md:text-[14px] mb-12 gap-8 md:w-1/3">
           <input
             type="text"
+            name="oruko"
+            value={info.oruko}
             placeholder="ENTER YOUR NAME"
             className="border-l-4 border-b-4 border-black w-[100%] pl-2 py-2"
+            onChange={infoHandler}
           />
           <input
             type="text"
+            name="email"
+            value={info.email}
             placeholder="ENTER YOUR EMAIL"
             className="border-l-4 border-b-4 border-black w-[100%] pl-2 py-2"
+            onChange={infoHandler}
           />
           <input
             type="text"
+            name="phone"
+            value={info.phone}
             placeholder="ENTER YOUR PHONE"
             className="border-l-4 border-b-4 border-black w-[100%] pl-2 py-2"
+            onChange={infoHandler}
           />
           <textarea
-            name="msg"
+            name="message"
             id=""
+            value={info.message}
             placeholder="YOUR MESSAGE"
             rows={5}
             className="border-l-4 border-b-4 border-black w-[100%] pl-2 py-2"
+            onChange={infoHandler}
           ></textarea>
 
-          <button className="border-x-2 border-black px-2 py-1">SUBMIT</button>
+          <button
+            className="px-2 py-1 border-black border-x-2"
+            onClick={sendEmail}
+          >
+            SUBMIT
+          </button>
         </div>
       </div>
     </div>
